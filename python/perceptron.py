cb5e54fb3022510184d109c1a3ec2c96f3d540f6
dongska <404050658@qq.com>
2024-12-31 15:58:42 +0800

    添加XOR函数实现，扩展逻辑门功能

--- python/perceptron.py
+++ python/perceptron.py
@@ -25,4 +25,10 @@ def NAND(x1,x2):
         return 1
     else:
         return 0
-print(OR(0,0))
+    
+def XOR(x1,x2):
+    s1 = NAND(x1,x2)
+    s2 = OR(x1,x2)
+    return AND(s1,s2)
+
+print(XOR(0,1))
